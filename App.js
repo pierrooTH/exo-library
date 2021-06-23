030bfe0ff1d4226ae8c647d73493f5179cf16a1e
Pierre Thouvenot <Pierre@MacBook-Air-de-pierre.local>
2021-06-23 15:06:26 +0200

    Initialization exercise library

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 
 export default function App() {
+  const [content, setContent] = useState('');
+  let books = [
+    {id: 1, name: 'Harry Potter et sa bite magique'},
+    {id: 2, name: '50 nuances de chattes'},
+    {id: 3, name: "La chatte de l'archiduchesse"},
+    {id: 4, name: "Pierre à l'âge de Pierre"},
+    {id: 5, name: 'Martine à la place'},
+    {id: 6, name: "Martine chez ta grand-mère"},
+    {id: 7, name: 'Harry Potter et sa braguette magmatique'}
+  ];
+  
+  const filteredBooks = books.filter(book => {
+    return book.name.toLowerCase().includes(content.toLowerCase())
+  })
+
+  const booksJSX = filteredBooks.map(book => {
+    return (
+      <Text key={book.id} style={styles.books}>
+        <AntDesign style={styles.icon} name="book" size={24} color="black" /> {book.name}
+      </Text>
+  )})
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.library}>Ma bibliothèque</Text>
+      <View style={styles.form}>
+      <TextInput style={styles.searchForm} value={content} onChangeText={(text) => {setContent(text)}} placeholder="nom du livre" />
+      <Button title='OK' />
+      </View>
+      {booksJSX}
     </View>
   );
 }
@@ -18,4 +44,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  library: {
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  searchForm: {
+    width: 200,
+    borderColor: 'black',
+    borderWidth: 2
+  },
+  form: {
+    margin: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  books: {
+    flexDirection: 'row',
+    margin: 10
+  },
+  icon: {
+    marginRight: 5
+  }
 });
